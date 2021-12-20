d0585949efbeda43e0fed8f9229974f69e0f357a
sriharshanaladala <83389426+sriharshanaladala@users.noreply.github.com>
2021-12-20 18:47:44 +0530

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit adee9f614a022deddfaad46a10036e7a76fdc0c8, reversing
    changes made to d12d55cd915da721cc69ba5b905e6ec0d65fb592.

--- main.py
+++ main.py
@@ -1,44 +1,25 @@
+import random
 from turtle import getturtle, getscreen
 
 t = getturtle()
 t.shape("turtle")
 t.color("green")
-print(t.position())
-t.goto(0, 100)
-# drawing a square with turtle module
-# t = ninja_turtle
-# for _ in range(4):
-#     t.forward(200)
-#     t.right(90)
-# drawing a dotted line with turtle module
-# for _ in range(10):
-#     t.forward(10)
-#     t.pendown()
-#     t  .forward(10)
-#     t.penup()
-for _ in range(3):
-    t.forward(100)
-    t.right(120)
-for _ in range(4):
-    t.forward(100)
-    t.right(90)
-for _ in range(5):
-    t.forward(100)
-    t.right(72)
-for _ in range(6):
-    t.forward(100)
-    t.right(60)
-for _ in range(7):
-    t.forward(100)
-    t.right(51)
-for _ in range(8):
-    t.forward(100)
-    t.right(45)
-for _ in range(9):
-    t.forward(100)
-    t.right(40)
-for _ in range(10):
-    t.forward(100)
-    t.right(36)
+t.home()
+colors = ["blue", "lime", "red", "dark magenta", "gold", "magenta", "dark green", "maroon",
+          ]
+
+
+def draw_shape(no_of_sides):
+    angle = 360/no_of_sides
+    for _ in range(no_of_sides):
+        t.forward(100)
+        t.right(angle)
+
+
+for shape_side in range(3, 11):
+    t.color(random.choice(colors))
+    draw_shape(shape_side)
+
+
 screen = getscreen()
 screen.exitonclick()
---
+++ turtle_random_walk.py
@@ -0,0 +1,16 @@
+from turtle import getturtle, getscreen
+import random
+tw = getturtle()
+tw.shape("turtle")
+tw.color("green")
+colors = ["blue", "lime", "red", "dark magenta", "gold", "magenta", "dark green", "maroon"]
+direction = [0, 90, 180, 270,]
+for _ in range(200):
+    tw.speed("fastest")
+    tw.pensize(20)
+    tw.width(7)
+    tw.color(random.choice(colors))
+    tw.forward(20)
+    tw.setheading(random.choice(direction))
+screen = getscreen()
+screen.exitonclick()
