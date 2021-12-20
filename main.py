7be50a3b6826c55b8835dfd78a9d2a493cff35d8
sriharshanaladala <83389426+sriharshanaladala@users.noreply.github.com>
2021-12-20 18:37:04 +0530

    instead of lenthy for loop code, by defining function making it short and readable

--- main.py
+++ main.py
@@ -16,29 +16,39 @@ t.goto(0, 100)
 #     t.pendown()
 #     t  .forward(10)
 #     t.penup()
-for _ in range(3):
-    t.forward(100)
-    t.right(120)
-for _ in range(4):
-    t.forward(100)
-    t.right(90)
-for _ in range(5):
-    t.forward(100)
-    t.right(72)
-for _ in range(6):
-    t.forward(100)
-    t.right(60)
-for _ in range(7):
-    t.forward(100)
-    t.right(51)
-for _ in range(8):
-    t.forward(100)
-    t.right(45)
-for _ in range(9):
-    t.forward(100)
-    t.right(40)
-for _ in range(10):
-    t.forward(100)
-    t.right(36)
+# for _ in range(3):
+#     t.forward(100)
+#     t.right(120)
+# for _ in range(4):
+#     t.forward(100)
+#     t.right(90)
+# for _ in range(5):
+#     t.forward(100)
+#     t.right(72)
+# for _ in range(6):
+#     t.forward(100)
+#     t.right(60)
+# for _ in range(7):
+#     t.forward(100)
+#     t.right(51)
+# for _ in range(8):
+#     t.forward(100)
+#     t.right(45)
+# for _ in range(9):
+#     t.forward(100)
+#     t.right(40)
+# for _ in range(10):
+#     t.forward(100)
+#     t.right(36)
+def draw_shape(no_of_sides):
+    angle = 360/no_of_sides
+    for _ in range(no_of_sides):
+        t.forward(100)
+        t.right(angle)
+
+for shape_side in range(3 , 11):
+    draw_shape(shape_side)
+
+
 screen = getscreen()
 screen.exitonclick()
